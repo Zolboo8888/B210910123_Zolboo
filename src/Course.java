80a04068f9b004b0352984f72ad3601a961747ca
Admin <Admin@DESKTOP-17CHQC0>
2023-05-03 15:52:16 +0800

    ng

---
+++ src/Course.java
@@ -0,0 +1,21 @@
+
+public class Course {
+
+		 String courseName;
+		
+	    Course(String courseName) {  
+	    	this.courseName = courseName;	
+	    }
+				
+		public String getCourseName() { 
+		return courseName;		
+		
+		}
+		
+		public void setCourseName(String courseName) {
+			this.courseName = courseName;
+			
+		
+	}
+
+}
---
+++ src/main.java
@@ -0,0 +1,19 @@
+
+public class main {
+			
+		
+	public static void main(String[] args) {
+		Course course = new Course("Software Construction");
+		course.setCourseName("Software Construction");
+		
+		
+
+		System.out.println(course.courseName);
+		System.out.println(course.getCourseName());
+			
+		
+		course.setCourseName("Программ хангамжийн бүтээлт");
+		System.out.println(course.getCourseName()); 
+						
+	}
+}
